ae91103ae1285a6ea51aa6295ba8d0886485bd11
c-hloehuang <chloeh2009@gmail.com>
2021-02-25 16:57:06 -0500

    hamster licc

--- main.js
+++ main.js
@@ -26,3 +26,23 @@ document.addEventListener('mousemove', (e) => {
         cursor.style.top = e.clientY + 'px';
 
 });
+
+let atScroll = false;
+let parallaxTitle = document.querySelectorAll(".parallax-title");
+
+const scrollProgress = () => {
+    atScroll = true;
+};
+
+const raf = () => {
+    if(atScroll) {
+        parallaxTitle.forEach((element, index) => {
+            element.style.transform = "translateX(" + window.scrollY / 8 + "%)";
+        });
+        atScroll = false;
+    }
+    requestAnimationFrame(raf);
+};
+
+requestAnimationFrame(raf);
+window.addEventListener("scroll", scrollProgress);
