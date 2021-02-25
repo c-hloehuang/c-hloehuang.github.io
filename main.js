50b8708909b867504d2c9d588f3d68e5e6321976
c-hloehuang <chloeh2009@gmail.com>
2021-02-24 23:25:52 -0500

    my deployment is not working why

--- main.js
+++ main.js
@@ -21,33 +21,7 @@ function toggle() {
     menuItems.classList.toggle('open');
 }
 
-
-    // const link = document.querySelector('.main-text');
 document.addEventListener('mousemove', (e) => {
         cursor.style.left = e.pageX + 'px';
         cursor.style.top = e.pageY + 'px';
 });
-
-    // const animate = function (e) {
-    //     const span = this.querySelector('span');
-    //     const { offsetX: x, offsetY: y } = e,
-    //     { offsetWidth: width, offsetHeight: height } = this,
-
-    //     move = 25,
-    //     xMove = x/width * (move * 2) - move,
-    //     yMove = y/heigh * (move * 2) - move;
-
-    //     span.style.transform = translate(${xMove}px, ${yMove}px);
-
-    //     if (e.type === 'mouseleave') span.style.transform = '';
-    // };
-
-    // const editCursor = e => {
-    //     const { clientX: x, clientY: y } = e;
-    //     cursor.style.left = x + 'px';
-    //     cursor.style.top = y + 'px';
-    // };
-
-    // link.forEach(b => b.addEventListener('mousemove', animate));
-    // link.forEach(b => b.addEventListener('mouseleave', animate));
-    // window.addEventListener('mousemove', editCursor);
