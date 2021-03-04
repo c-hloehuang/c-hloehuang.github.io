e79cded05d969b4549c566511a2b3ab649fbd94e
c-hloehuang <chloeh2009@gmail.com>
2021-03-04 14:59:55 -0500

    smooth scroll

--- main.js
+++ main.js
@@ -45,3 +45,20 @@ const raf = () => {
 
 requestAnimationFrame(raf);
 window.addEventListener("scroll", scrollProgress);
+
+const links = document.querySelectorAll(".header ul a");
+ 
+for (const link of links) {
+  link.addEventListener("click", clickHandler);
+}
+ 
+function clickHandler(e) {
+  e.preventDefault();
+  const href = this.getAttribute("href");
+  const offsetTop = document.querySelector(href).offsetTop;
+ 
+  scroll({
+    top: offsetTop,
+    behavior: "smooth"
+  });
+}
